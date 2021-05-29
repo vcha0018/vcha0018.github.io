cf01c0b4a51461fcdbd883a1bf37113f763abfe0
Vivek Chaudhari <75250342+vcha0018@users.noreply.github.com>
2021-05-29 14:48:18 +1000

    fit5225 project files

---
+++ index.js
@@ -0,0 +1,305 @@
+// var MDCTabBar = require('@material/tab-bar');
+// import {MDCTabBar} from '.material/tab-bar';
+
+const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
+[].map.call(document.querySelectorAll(selector), function (el) {
+    return new mdc.ripple.MDCRipple(el);
+});
+[].map.call(document.querySelectorAll('.mdc-text-field'), function (el) {
+    return new mdc.textField.MDCTextField(el);
+});
+const main_tabBar = mdc.tabBar.MDCTabBar.attachTo(document.querySelector('.main_tabbar'));
+const child_tabBar = mdc.tabBar.MDCTabBar.attachTo(document.querySelector('.child_tabbar'));
+const snackbar = new mdc.snackbar.MDCSnackbar(document.querySelector('.mdc-snackbar'));
+snackbar.timeoutMs = 4000;
+snackbar.closeOnEscape = true;
+
+$("#uploadImgBtn").prop('disabled', true);
+$("#searchbyTagBtn").prop('disabled', true);
+$("#searchImgBtn").prop('disabled', true);
+$("#viewImgBtn").prop('disabled', true);
+$("#updateImgBtn").prop('disabled', true);
+$("#deleteImgBtn").prop('disabled', true);
+
+
+$("#query_div").css("display", "none");
+$("#upload_div").css("display", "block");
+main_tabBar.listen('MDCTabBar:activated', function (event) {
+    // let tab = tabs[event.detail.index];
+    // console.log(tab.children[0].children[1].textContent, 'tab activated');
+    if (event.detail.index == 0) {
+        $("#primaryTxt").text("Select an Image and than click Upload button.")
+        $("#query_div").css("display", "none");
+        $("#upload_div").css("display", "block");
+
+    }
+    else if (event.detail.index == 1) {
+        $("#primaryTxt").text("Query Operaions")
+        $("#upload_div").css("display", "none");
+        $("#query_div").css("display", "block");
+
+    }
+});
+$("#search_by_image_div").css("display", "none");
+$("#update_tags_div").css("display", "none");
+$("#delete_image_div").css("display", "none");
+$("#view_image_div").css("display", "none");
+$("#search_by_tags_div").css("display", "block");
+main_tabBar.activateTab(0);
+child_tabBar.listen('MDCTabBar:activated', function (event) {
+    if (event.detail.index == 0) {
+        $("#search_by_image_div").css("display", "none");
+        $("#update_tags_div").css("display", "none");
+        $("#delete_image_div").css("display", "none");
+        $("#view_image_div").css("display", "none");
+        $("#search_by_tags_div").css("display", "block");
+
+    } else if (event.detail.index == 1) {
+        $("#search_by_tags_div").css("display", "none");
+        $("#update_tags_div").css("display", "none");
+        $("#delete_image_div").css("display", "none");
+        $("#view_image_div").css("display", "none");
+        $("#search_by_image_div").css("display", "block");
+
+    } else if (event.detail.index == 2) {
+        $("#search_by_tags_div").css("display", "none");
+        $("#delete_image_div").css("display", "none");
+        $("#search_by_image_div").css("display", "none");
+        $("#update_tags_div").css("display", "none");
+        $("#view_image_div").css("display", "block");
+
+    } else if (event.detail.index == 3) {
+        $("#search_by_tags_div").css("display", "none");
+        $("#search_by_image_div").css("display", "none");
+        $("#view_image_div").css("display", "none");
+        $("#delete_image_div").css("display", "none");
+        $("#update_tags_div").css("display", "block");
+
+    } else if (event.detail.index == 4) {
+        $("#search_by_tags_div").css("display", "none");
+        $("#search_by_image_div").css("display", "none");
+        $("#view_image_div").css("display", "none");
+        $("#update_tags_div").css("display", "none");
+        $("#delete_image_div").css("display", "block");
+
+    }
+});
+child_tabBar.activateTab(0);
+
+const selectFileBtn = document.getElementById("selectFileBtn");
+
+$("#selectFileBtn").bind("click", selectFileBtnOnClick);
+$("#selectFileBtn2").bind("click", selectFile2BtnOnClick);
+$("#uploadImgBtn").bind("click", uploadImgBtnOnClick);
+$("#selectFile").bind("change", selectFileChanged);
+$("#selectFile2").bind("change", selectFile2Changed);
+$("#searchbyTagBtn").bind("click", searchbyTagBtnOnClick);
+$("#searchImgBtn").bind("click", searchImgBtnOnClick);
+$("#updateImgBtn").bind("click", updateImgBtnOnClick);
+$("#deleteImgBtn").bind("click", deleteImgBtnOnClick);
+
+$("#input_tags_search").bind("input", input_tags_searchOnInputChange);
+$("#input_url_view_image").bind("input", input_url_view_imageOnInputChange);
+$("#input_url_update").bind("input", input_url_updateOnInputChange);
+$("#input_tags_update").bind("input", input_tags_updateOnInputChange);
+$("#input_img_delete").bind("input", input_img_deleteOnInputChange);
+
+function selectFileBtnOnClick() {
+    $("#selectFile").trigger("click");
+}
+
+function selectFile2BtnOnClick() {
+    $("#selectFile2").trigger("click");
+}
+
+function uploadImgBtnOnClick() {
+    $("#uploadImgBtn").prop('disabled', true);
+    var image_name = "image_" + new Date().getTime();
+    var image_file = $("#selectFile")[0].files[0];
+    var content_type = image_file.type;
+    console.log(content_type);
+    console.log(image_name);
+    if (content_type.includes("image/")) {
+        var formData = new FormData();
+        var put_url = `https://lr00fm7ci7.execute-api.us-east-1.amazonaws.com/api_v1/tasks/image?key=${image_name}.${content_type.substr(6)}`
+        console.log(put_url);
+        formData.append("file", image_file);
+        $.ajax({
+            type: 'PUT',
+            url: put_url,
+            data: formData,
+            cache: false,
+            contentType: content_type,
+            processData: false,
+            success: handleResponse,
+            error: handleResponse
+        });
+
+        function handleResponse(response) {
+            console.log(response);
+            var message = response.status == 200 ? "Upload Successful." : "Upload failed.";
+            snackbar.labelText = message;
+            snackbar.open();
+            $("#uploadImgBtn").prop('disabled', false);
+        }
+    } else {
+        snackbar.labelText = "Not a valid image.";
+        snackbar.open();
+    }
+}
+
+function selectFileChanged() {
+    readURL(this, '#blah');
+    $("#uploadImgBtn").prop('disabled', false);
+
+}
+
+function selectFile2Changed() {
+    readURL(this, '#blah2');
+    $("#searchImgBtn").prop('disabled', false);
+}
+
+function searchbyTagBtnOnClick() {
+
+    $("#searchbyTagBtn").prop('disabled', true);
+    input_txt = $("#input_tags_search").val().trim();
+    if (input_txt != null && input_txt != "") {
+        if (input_txt.includes(",")) {
+            var input_tags = input_txt.split(",").map(function (value) {
+                return value.trim();
+            });
+        } else {
+            input_tags = [input_txt];
+        }
+    }
+    if (input_tags != null && input_tags != undefined && input_tags.length > 0 &&
+        !input_tags.includes(undefined) && !input_tags.includes("")) {
+        console.log(input_tags);
+        var json_data = {
+            "tags": input_tags
+        };
+        var post_url = `https://lr00fm7ci7.execute-api.us-east-1.amazonaws.com/api_v1/tasks/query`
+        $.ajax({
+            method: 'POST',
+            url: post_url,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            // beforeSend: function (xhr) {
+            //     // xhr.setRequestHeader("Authorization", "Basic " + btoa(""));
+            // },
+            data: JSON.stringify(json_data),
+            success: handleResponse,
+            error: handleResponse
+        });
+
+        function handleResponse(response) {
+            console.log(response);
+            var message = response.status == 200 ? "Upload Successful." : "Upload failed.";
+            snackbar.labelText = message;
+            snackbar.open();
+            populateList("#imglist1", ["Link 1", "Link 2", "Link 3"]);
+            $("#searchbyTagBtn").prop('disabled', false);
+        }
+    } else {
+        snackbar.labelText = "Not a valid tag string.";
+        snackbar.open();
+    }
+}
+
+function searchImgBtnOnClick() {
+
+}
+
+function updateImgBtnOnClick() {
+
+}
+
+function deleteImgBtnOnClick() {
+
+}
+
+function input_tags_searchOnInputChange() {
+    if ($(this).val() != undefined && $(this).val() != null && $(this).val() != "") {
+        $("#searchbyTagBtn").prop('disabled', false);
+    } else {
+        $("#searchbyTagBtn").prop('disabled', true);
+    }
+}
+
+function input_url_view_imageOnInputChange() {
+    if ($(this).val() != undefined && $(this).val() != null && $(this).val() != "") {
+        $("#viewImgBtn").prop('disabled', false);
+
+    } else {
+        $("#viewImgBtn").prop('disabled', true);
+
+    }
+}
+
+function input_url_updateOnInputChange() {
+    tags_input_val = $("#input_tags_update").val();
+    if ($(this).val() != undefined && $(this).val() != null && $(this).val() != "" &&
+        tags_input_val != undefined && tags_input_val != null && tags_input_val != "") {
+        $("#updateImgBtn").prop('disabled', false);
+
+    } else {
+        $("#updateImgBtn").prop('disabled', true);
+
+    }
+}
+
+function input_tags_updateOnInputChange() {
+    url_input_val = $("#input_url_update").val();
+    if ($(this).val() != undefined && $(this).val() != null && $(this).val() != "" &&
+        url_input_val != undefined && url_input_val != null && url_input_val != "") {
+        $("#updateImgBtn").prop('disabled', false);
+
+    } else {
+        $("#updateImgBtn").prop('disabled', true);
+
+    }
+}
+
+function input_img_deleteOnInputChange() {
+    if ($(this).val() != undefined && $(this).val() != null && $(this).val() != "") {
+        $("#deleteImgBtn").prop('disabled', false);
+
+    } else {
+        $("#deleteImgBtn").prop('disabled', true);
+
+    }
+}
+
+function readURL(input, imgid) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $(imgid).attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function populateList(list_id, list_items) {
+    $(list_id).empty();
+    if (list_items != null && list_items.length > 0) {
+        list_items.forEach(function (item, index) {
+            $(list_id).append(`
+            <li class="mdc-list-item list_item">
+                <span class="mdc-list-item__ripple"></span>
+                <span class="mdc-list-item__text">${item}</span>
+            </li>`);
+        });
+        $(list_id).css('display', 'block');
+    }
+}
+
+function generateNewChip(id, text) {
+    `<div class="chip">
+    ${text}
+    <span class="closebtn" onclick="this.parentElement.style.display='none'">&times;</span>
+    </div>`
+}
